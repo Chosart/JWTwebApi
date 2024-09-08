0b8b16e31923e24966294a34b968cb1a2b84a937
Chosart <164737726+Chosart@users.noreply.github.com>
2024-09-08 09:47:37 +0300

    Create the JSON Web Token

--- JWTwebApi/Controllers/AuthController.cs
+++ JWTwebApi/Controllers/AuthController.cs
@@ -1,6 +1,11 @@
 ﻿using JWTwebApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace JWTwebApi.Controllers
 {
@@ -9,6 +14,12 @@ namespace JWTwebApi.Controllers
     public class AuthController : ControllerBase
     {
         public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         [HttpPost("register")]
         public ActionResult<User> Register(UserDto request)
@@ -21,22 +32,52 @@ namespace JWTwebApi.Controllers
 
             return Ok(user);
         }
-        
+
         [HttpPost("login")]
         public ActionResult<User> Login(UserDto request)
         {
-            if(user.UserName != request.UserName)
+            if (user.UserName != request.UserName)
             {
                 return BadRequest("User not found.");
             }
 
-            if(!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
             {
                 return BadRequest("Wrong password.");
             }
 
-            return Ok(user);
+            string token = CreateToken(user);
+
+            return Ok(token);
         }
 
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim> {
+        new Claim(ClaimTypes.Name, user.UserName)
+    };
+
+            // Отримав ключ з конфігурації
+            var keyString = _configuration.GetSection("AppSettings:Token").Value;
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(keyString));
+
+            // Перевірив довжину ключа
+            if (key.Key.Length < 64)
+            {
+                throw new ArgumentException("Key length must be at least 64 bytes.");
+            }
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
     }
 }
